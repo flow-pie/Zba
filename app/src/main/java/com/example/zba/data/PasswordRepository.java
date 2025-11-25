1cba26b9a89bf448b8a5f4523ef371c4e4c54037
flow-pie <startabase@gmail.com>
2025-11-25 07:40:44 +0300

    Refactor `PasswordRepository` to use Firebase Firestore for all CRUD operations, replacing the previous in-memory `ArrayList`.
    - Implement `addPassword`, `removePassword`, and `updatePassword` methods to interact with the Firestore database.
    - Introduce a realtime listener (`addSnapshotListener`) to fetch and display password updates automatically.
    - Add logging for success and failure of database operations.

--- app/src/main/java/com/example/zba/data/PasswordRepository.java
+++ app/src/main/java/com/example/zba/data/PasswordRepository.java
@@ -1,46 +1,88 @@
 package com.example.zba.data;
 
 import android.os.Build;
+import android.util.Log;
 
 import com.example.zba.models.PasswordItem;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class PasswordRepository {
+    private static final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    private static final String TAG = "PasswordRepository";
     public static final List <PasswordItem> passwordList = new ArrayList<>();
 
-    public static List<PasswordItem> getPasswords() {
-        return passwordList;
-    }
-    public static void addPassword(PasswordItem item){
-        passwordList.add(item);
+    //realtime listener
+    public interface  PasswordListener {
+        void onPasswordLoaded(List<PasswordItem> items);
     }
 
-    public static void removePassword(String id){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            passwordList.removeIf(item -> item.getId().equals(id));
-        }else {
-            Iterator<PasswordItem> iterator = passwordList.iterator();
-            while (iterator.hasNext()) {
-                PasswordItem item = iterator.next();
-                if (item.getId().equals(id)) {
-                    iterator.remove();
+    public void getPasswords(PasswordListener listener) {
+        db.collection("users")
+                .document(uid)
+                .collection("passwords")
+                .addSnapshotListener((snapshot, error) -> {
+                    if (error != null) {
+                        Log.e(TAG, "Snapshot error", error);
+                        return;
+                    }
+
+                List<PasswordItem> list = new ArrayList<>();
+                if(snapshot != null) {
+                    for (DocumentSnapshot doc : snapshot.getDocuments()){
+                        PasswordItem item = doc.toObject(PasswordItem.class);
+                        list.add(item);
+                    }
                 }
-            }
-        }
+                listener.onPasswordLoaded(list);
+                });
     }
-    public static boolean updatePassword(String id, String newAppName, String newUsername, String newPassword) {
-        for (PasswordItem item : passwordList) {
-            if (item.getId().equals(id)) {
-                item.setAppName(newAppName);
-                item.setUserName(newUsername);
-                item.setPassword(newPassword);
-                return true;
-            }
-        }
-        return false;
+    public static void addPassword(PasswordItem item) {
+        String id = db.collection("users")
+                .document(uid)
+                .collection("passwords")
+                .document()
+                .getId();
+
+        item.setId(id);
+
+        db.collection("users")
+                .document(uid)
+                .collection("passwords")
+                .document(id)
+                .set(item)
+                .addOnSuccessListener(v -> Log.d(TAG, "Password saved"))
+                .addOnFailureListener(e -> Log.e(TAG, "Save failed", e));
+    }
+
+    public static void removePassword(String id) {
+        db.collection("users")
+                .document(uid)
+                .collection("passwords")
+                .document(id)
+                .delete()
+                .addOnSuccessListener(v -> Log.d(TAG, "Deleted"))
+                .addOnFailureListener(e -> Log.e(TAG, "Delete failed", e));
+    }
+    public static void updatePassword(String id, String newAppName, String newUsername, String newPassword) {
+        db.collection("users")
+                .document(uid)
+                .collection("passwords")
+                .document(id)
+                .update(
+                        "appName", newAppName,
+                        "userName", newUsername,
+                        "password", newPassword
+                )
+                .addOnSuccessListener(v -> Log.d(TAG, "Updated"))
+                .addOnFailureListener(e -> Log.e(TAG, "Update failed", e));
     }
 
     public static void clear(){
